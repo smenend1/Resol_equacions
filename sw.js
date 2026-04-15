10847c0ce3ad02a973f8becab1b91da0bc4ce77f
smenend1 <smenend1@xtec.cat>
2026-04-15 08:59:50 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,44 +3,8 @@ const ASSETS = [
   './',
   './index.html',
   './manifest.json',
+  './logo.svg',  // <--- Afegeix aquesta línia
   'https://cdn.jsdelivr.net/npm/chart.js',
   'https://cdnjs.cloudflare.com/ajax/libs/mathjs/11.8.0/math.js'
 ];
-
-// Instal·lació: Guardem els fitxers a la memòria cau
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => {
-        console.log('Cache obert correctament');
-        return cache.addAll(ASSETS);
-      })
-      .catch(err => console.error('Error en el cache inicial:', err))
-  );
-});
-
-// Activació: Esborrem versions antigues de la cache
-self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.map((key) => {
-          if (key !== CACHE_NAME) {
-            console.log('Netejant cache antiga:', key);
-            return caches.delete(key);
-          }
-        })
-      );
-    })
-  );
-});
-
-// Fetch: Estratègia "Cache First"
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => {
-      // Si el tenim a la cache, el servim. Si no, anem a buscar-lo a la xarxa.
-      return res || fetch(e.request);
-    })
-  );
-});
+// ... la resta del codi del sw.js és igual
